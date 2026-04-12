eae3547096f43b88fc3b16313bd6f5a067f626c2
abhishek kumar yadav <ay997326@gmail.com>
2026-04-12 23:12:00 +0530

    Time: 6 ms (77.86%), Space: 19.7 MB (43.17%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        res = []
+        def backtrack(start, path, target):
+            if target == 0:
+                res.append(path[:])
+                return
+            if target < 0:
+                return
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, target - candidates[i])
+                path.pop()    
+        backtrack(0, [], target)
+        return res
